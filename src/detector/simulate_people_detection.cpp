6222e7f0ae7365d6b7cb76fc702479dd8e5fc3b7
JCLArriaga5 <carlos_gara_1507@hotmail.com>
2020-04-18 19:28:58 -0500

    Update code for only visualization in PCL

--- src/detector/simulate_people_detection.cpp
+++ src/detector/simulate_people_detection.cpp
@@ -15,8 +15,7 @@ typedef pcl::PointXYZRGBA PointT;
 typedef sensor_msgs::PointCloud2 PointCloudT;
 
 // PCL viewer //
-// boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
-pcl::visualization::PCLVisualizer viewer("PCL Viewer");
+boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
 
 // Mutex: //
 boost::mutex cloud_mutex;
@@ -24,17 +23,23 @@ ros::Publisher pub;
 
 enum { COLS = 640, ROWS = 480 };
 
-int print_help()
+boost::shared_ptr<pcl::visualization::PCLVisualizer> Visualization ()
 {
-  std::cout << "*******************************************************" << std::endl;
-  std::cout << "Ground based people detection app options:" << std::endl;
-  std::cout << "   --help    <show_this_help>" << std::endl;
-  std::cout << "   --svm     <path_to_svm_file>" << std::endl;
-  std::cout << "   --conf    <minimum_HOG_confidence (default = -1.5)>" << std::endl;
-  std::cout << "   --min_h   <minimum_person_height (default = 1.3)>" << std::endl;
-  std::cout << "   --max_h   <maximum_person_height (default = 2.3)>" << std::endl;
-  std::cout << "*******************************************************" << std::endl;
-  return 0;
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("PCL Viewer"));
+  viewer->addCoordinateSystem (1.0);
+  viewer->setCameraPosition(0,0,-2,0,-1,0,0);
+  viewer->initCameraParameters ();
+  return viewer;
+}
+
+void Visualization_update(boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer,
+  pcl::PointCloud<PointT>::ConstPtr cloud)
+{
+  // Create PCL Viewer Window
+  pcl::visualization::PointCloudColorHandlerRGBField<PointT> rgb(cloud);
+  viewer->removePointCloud();
+  viewer->addPointCloud<PointT> (cloud, rgb);
+  viewer->spinOnce();
 }
 
 void cloud_cb_ (const PointCloudT::ConstPtr& input)
@@ -43,48 +48,23 @@ void cloud_cb_ (const PointCloudT::ConstPtr& input)
   pcl::PointCloud<PointT> callback_cloud;
   pcl::fromROSMsg (*input, callback_cloud);
 
-  pcl::PointCloud<PointT>::Ptr cloud;
-
-  cloud_mutex.lock ();    // for not overwriting the point cloud from another thread
-  *cloud = callback_cloud;
-  bool new_cloud_available_flag = true;
-  cloud_mutex.unlock ();
+  pcl::PointCloud<PointT>::Ptr _cloud_ (new pcl::PointCloud<PointT>);
+  *_cloud_ = callback_cloud;
 
   // setting alpha = 1.0 for rviz
   for (size_t i = 0; i < callback_cloud.points.size(); ++i){
     callback_cloud.points[i].a = 255;
   }
+  pub.publish(callback_cloud);
 
-  pub.publish(cloud);
-}
-
-struct callback_args{
-  // structure used to pass arguments to the callback function
-  pcl::PointCloud<PointT>::Ptr clicked_points_3d;
-  pcl::visualization::PCLVisualizer::Ptr viewerPtr;
-};
-
-void
-pp_callback (const pcl::visualization::PointPickingEvent& event, void* args)
-{
-  struct callback_args* data = (struct callback_args *)args;
-  if (event.getPointIndex () == -1)
-    return;
-  PointT current_point;
-  event.getPoint(current_point.x, current_point.y, current_point.z);
-  data->clicked_points_3d->points.push_back(current_point);
-  // Draw clicked points in red:
-  pcl::visualization::PointCloudColorHandlerCustom<PointT> red (data->clicked_points_3d, 255, 0, 0);
-  data->viewerPtr->removePointCloud("clicked_points");
-  data->viewerPtr->addPointCloud(data->clicked_points_3d, red, "clicked_points");
-  data->viewerPtr->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 10, "clicked_points");
-  std::cout << current_point.x << " " << current_point.y << " " << current_point.z << std::endl;
+  // Display pointcloud:
+  Visualization_update(viewer, _cloud_);
 }
 
 int main (int argc, char** argv)
 {
-  if(pcl::console::find_switch (argc, argv, "--help") || pcl::console::find_switch (argc, argv, "-h"))
-        return print_help();
+  // Initialization of PCL Viewer
+  viewer = Visualization();
 
   // Initialize ROS
   ros::init (argc, argv, "simulate_people_detection");
@@ -101,15 +81,4 @@ int main (int argc, char** argv)
 
   // Spin
   ros::spin ();
-
-  // Algorithm parameters:
-  std::string svm_filename = "/utils/people/data/trainedLinearSVMForPeopleDetectionWithHOG.yaml";
-  float min_confidence = -1.5;
-  float voxel_size = 0.06;
-  Eigen::Matrix3f rgb_intrinsics_matrix;
-    rgb_intrinsics_matrix << 525, 0.0, 316.7, 0.0, 525, 238.5, 0.0, 0.0, 1.0; // Kinect RGB camera intrinsics
-
-  // Read if some parameters are passed from command line:
-  pcl::console::parse_argument (argc, argv, "--svm", svm_filename);
-  pcl::console::parse_argument (argc, argv, "--conf", min_confidence);
 }
